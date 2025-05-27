85cb9277ad5c2629259d310d7e422bf08a6f3906
sarathir-dev <sarathir.dev@gmail.com>
2025-05-27 21:26:42 +0530

    Added config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
 # Configurations & hyperparameters
+
+import os
+import torch
+
+DATA_PATH = "datasets/hockey_fight"
+BATCH_SIZE = 8
+NUM_EPOCHS = 30
+LR = 1e-4
+IMG_SIZE = (128, 129)
+SEQ_LEN = 16
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
