e2377482dbc94c2472987fd7dd4100263104b5dc
sarathir-dev <sarathir.dev@gmail.com>
2025-05-28 10:35:03 +0530

    Updated configs & hyper-parameters

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 import os
 import torch
 
-DATA_PATH = "datasets/hockey_fight"
-BATCH_SIZE = 8
+DATA_PATH = "./data/violence_dataset/hockey_fight"
+BATCH_SIZE = 16
 NUM_EPOCHS = 30
 LR = 1e-4
-IMG_SIZE = (128, 129)
+IMG_SIZE = (128, 128)
 SEQ_LEN = 16
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
